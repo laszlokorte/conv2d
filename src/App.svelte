9d3cd3531e2b5777a22795549fd438aad5d40692
Laszlo Korte <9korte@informatik.uni-hamburg.de>
2023-08-07 16:48:19 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,356 @@
+<script>
+	import Canvas from './Canvas.svelte'
+	
+
+	let brush = {
+		active: false,
+		value: 1,
+		size: 1,
+		hardness: 0,
+		element: null,
+		prev:{x:null,y:null},
+		current:{x:null,y:null},
+	}
+
+	let inputImage = {
+		size: {x:20,y:20},
+		values: Array(20).fill(0).map(v=>Array(20).fill(v)),
+	};
+
+	let inputRange;
+
+	let filter = {
+		padding: {left: 0,right:0,top:0,bottom:0},
+		mask: {size: {x:5,y:5}, values: Array(5).fill(0).map(v=>Array(5).fill(v)),},
+		paddingType: 'zero',
+		function: 'identity',
+		flip: true,
+	};
+
+	const paddingTypes = {
+		zero: (original, x, y) => {
+			if(original.values && y >= 0 && y < original.values.length) {
+				if(x >= 0 && x < original.values[0].length) {
+					return original.values[y][x]
+				} else {
+					return 0
+				}
+			} else {
+				return 0
+			}
+		},
+		replicate: (original, x, y) => {
+			if(y<0) y = 0;
+			if(x<0) x = 0;
+			if(y>=original.values.length) y = original.values.length-1;
+			if(x>=original.values[y].length) x = original.values[y].length-1;
+			return original.values[y][x]
+		},
+		mirror: (original, x, y) => {
+			const modY = (original.size.y)*2
+			const modX = (original.size.x)*2
+
+			const doubleY = ((y % modY)+modY)%modY
+			const doubleX = ((x % modX)+modX)%modX
+
+			const yover = (doubleY/original.size.y)<<0
+			const xover = (doubleX/original.size.x)<<0
+
+			y = ((original.size.y-yover+(yover?-1:1)*y)%original.size.y+original.size.y)%original.size.y
+			x = ((original.size.x-xover+(xover?-1:1)*x)%original.size.x+original.size.x)%original.size.x
+			
+			return original.values[y][x]
+		},
+		reflect: (original, x, y) => {
+			const modY = (original.size.y)*2
+			const modX = (original.size.x)*2
+
+			const doubleY = ((y % modY)+modY)%modY
+			const doubleX = ((x % modX)+modX)%modX
+
+			const yover = (doubleY/original.size.y)<<0
+			const xover = (doubleX/original.size.x)<<0
+
+			y = ((original.size.y-2*yover+(yover?-1:1)*(y))%(original.size.y-yover)+original.size.y)%(original.size.y-yover)
+			x = ((original.size.x-2*xover+(xover?-1:1)*(x))%(original.size.x-xover)+original.size.x)%(original.size.x-xover)
+			
+			return original.values[y][x]
+		},
+		cyclic: (original, x, y) => {
+			x = (x%original.size.x + original.size.x)%original.size.x
+			y = (y%original.size.y + original.size.y)%original.size.y
+			
+			return original.values[y][x]
+		},
+	}
+
+	const functions = {
+		identity: (x) => x,
+		ceil: (x) => Math.ceil(x),
+		floor: (x) => Math.floor(x),
+		abs: (x) => Math.abs(x),
+		round: (x) => Math.round(x),
+		relu: (x) => x>=0?x:0,
+	}
+
+	$: paddedImage = {
+		size: {
+			x: inputImage.size.x + filter.padding.left + filter.padding.right,
+			y: inputImage.size.y + filter.padding.top + filter.padding.bottom,
+		},
+		values: Array(inputImage.size.y + filter.padding.top + filter.padding.bottom).fill(0).map((v,y) => 
+			Array(inputImage.size.x + filter.padding.left + filter.padding.right).fill(0).map((_,x) => paddingTypes[filter.paddingType](inputImage, x-filter.padding.left,y-filter.padding.top))
+			)
+	}
+
+	$: filterNorm = Math.abs(filter.mask.values.flat().reduce((a,b)=>a+b, 0)) || 1
+
+	$: filteredImage = {
+		size: {
+			x: paddedImage.size.x - filter.mask.size.x + 1,
+			y: paddedImage.size.y - filter.mask.size.y + 1,
+		},
+		values: Array(paddedImage.size.y - filter.mask.size.y + 1).fill(0).map((v,y) => 
+			Array(paddedImage.size.x - filter.mask.size.x + 1).fill(0).map((_,x) => 
+				functions[filter.function](filter.mask.values.flatMap((row, fy) => row.map((w, fx) => paddedImage.values[y+(filter.flip?(filter.mask.size.y-fy-1):fy)][x+(filter.flip?(filter.mask.size.x-fx-1):fx)]*w)).reduce((a,b)=>a+b, 0)/filterNorm)
+			)
+		)
+	}
+
+	$: filteredImageMin = Math.min.apply(Math, filteredImage.values.flat())
+	$: filteredImageMax = Math.max.apply(Math, filteredImage.values.flat())
+	$: paddedImageMin = Math.min.apply(Math, paddedImage.values.flat())
+	$: paddedImageMax = Math.max.apply(Math, paddedImage.values.flat())
+</script>
+
+<style>
+	.intensity {
+		fill: hsl(0deg 0% calc(var(--intensity, 0.5)*100%));
+		accent-color: hsl(0deg 0% calc(var(--intensity, 0.5)*100%));
+	}
+
+	.options {
+		display: grid;
+		grid-template-columns: auto 3em auto;
+		justify-content: start;
+		gap: 1em;
+	}
+	.options-list {
+		display: grid;
+		grid-template-columns: auto auto;
+		justify-content: start;
+		gap: 1em;
+	}
+
+  
+   .option-container {
+		display: flex;
+		 flex-direction: column;
+		gap: 1em;
+		border: none;
+		background: #333;
+		color: #fff;
+	}
+
+	.option-container-label {
+		display: inline-block;
+		padding: 2px 0.5em;
+		background: inherit;
+	}
+
+	dd, dt, dl {
+		margin: 0;
+		padding: 0;
+	}
+
+	dt {
+		font-size: 0.8em;
+		align-self: center;
+	}
+
+	section {
+		display: grid;
+		grid-template-columns: auto auto;
+		justify-content: start;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+	}
+	.padding-options {
+		display: grid;
+		grid-template-rows: auto auto;
+		gap: 0 0.2em;
+	}
+
+	.padding-options > dt {
+		grid-row: 2;
+		text-align: center;
+		font-size: 0.8em;
+	}
+	.padding-options > dd {
+		grid-row: 1;
+		text-align: center;
+	}
+
+	.image-container {
+		background: #000;
+		color: #fff;
+		padding: 0.8em;
+		display: flex;
+		flex-direction: column;
+		gap: 0.3em;
+	}
+
+	h2 {
+		margin: 0;
+	}
+
+	.brush {
+		position: fixed;
+		top: 1em;
+		right: 1em;
+		margin-left: auto;
+	}
+	
+	.image-array {
+		max-width: 18em;
+		max-height: 24em;
+		width: 100%;
+		min-width: 20em;
+		height: auto;
+		border: 1px solid white;
+	}
+
+	.filter {
+		display: flex;
+		align-items: start;
+		gap: 1em;
+	}
+
+	.padding-field {
+		width: 100%;
+		flex-grow: 1;
+		flex-shrink: 1;
+	}
+
+	select, input {
+		margin: 0;
+	}
+
+	h2 {
+		font-size: 1.2em;
+	}
+</style>
+
+<div class="container">
+	
+<section class="brush">
+	<fieldset class="option-container">
+	<legend class="option-container-label">Brush</legend>
+	<dl class="options">
+	<dt>Brightness </dt>
+	<dd>	<svg style="border:1px solid black" viewBox="0 0 10 10" width={10} height={10}><rect x="0" y="0" width="10" height="10" style:--intensity={brush.value} fill="magenta" class="intensity" /></svg>
+</dd>
+	<dd><input style:--intensity={brush.value} class="intensity" type="range" min="0" max="1" step="0.05" bind:value={brush.value}/></dd>
+	<dt>Size</dt>
+	<dd>{brush.size}</dd>
+	<dd><input type="range" min="1" max="6" step="1" bind:value={brush.size}/></dd>
+	<dt>Hardness</dt>
+	<dd>{Math.round(brush.hardness*100)}%</dd>
+	<dd><input type="range" min="0" max="1" step="0.05" bind:value={brush.hardness}/></dd>
+</dl>
+</fieldset>
+
+</section>
+
+
+<div class="image-container">
+	<h2>Input Image</h2>
+	<Canvas bind:range={inputRange} bind:image={inputImage} brush={brush} />
+</div>
+
+<div>	
+	<div class="filter">
+<div class="image-container">
+	<h2>Padding</h2>
+	<fieldset class="option-container">
+	<legend class="option-container-label">Options</legend>
+		<dl class="options">
+			<dt>Type</dt>
+			<dd>
+				<select bind:value={filter.paddingType}>
+					{#each Object.keys(paddingTypes) as type}
+						<option>{type}</option>
+					{/each}
+				</select>
+			</dd>
+		</dl>
+	<dl class="padding-options">
+	<dt>Top</dt>
+	<dd><input class="padding-field" type="number" size="5" min="0" max="50" step="1" bind:value={filter.padding.top} /></dd>
+	<dt>Right</dt>
+	<dd><input class="padding-field" type="number" size="5" min="0" max="50" step="1" bind:value={filter.padding.right} /></dd>
+	<dt>Bottom</dt>
+	<dd><input class="padding-field" type="number" size="5" min="0" max="50" step="1" bind:value={filter.padding.bottom} /></dd>
+	<dt>Left</dt>
+	<dd><input class="padding-field" type="number" size="5" min="0" max="50" step="1" bind:value={filter.padding.left} /></dd>
+	</dl>
+	</fieldset>
+
+	<div>
+			<h2>Padded Image</h2>
+		<svg role="presentation" class="image-array no-cursor" viewBox="0 0 {paddedImage.size.x} {paddedImage.size.y}" width="{paddedImage.size.x}" height="{paddedImage.size.y}" preserveAspectRatio="meet xMidYMid">
+		{#each paddedImage.values as row, y}
+			{#each row as value, x}
+				<rect pointer-events="none" class="intensity" style:--intensity={(value-inputRange)/(1-inputRange)} fill="magenta" image-rendering="crisp-edges" stroke="#abb3" {x} {y} width="1" height=1  vector-effect="non-scaling-stroke" stroke-width="1px"></rect>
+			{/each}
+		{/each}
+			<rect stroke="cyan" fill="none" x={filter.padding.left} y={filter.padding.top} width={inputImage.size.x} height={inputImage.size.y} vector-effect="non-scaling-stroke" stroke-width="1px"></rect>
+		</svg>
+	</div>
+
+	
+	
+</div>
+<div class="image-container">
+	<h2>Filter</h2>
+	<fieldset class="option-container">
+	<legend class="option-container-label">Operation</legend>
+		<dl class="options-list">
+			<dt>Vector Operation</dt>
+			<dd>
+				<select bind:value={filter.flip}>
+					<option value={true}>Convolution</option>
+					<option value={false}>Correlation</option>
+				</select>
+			</dd>
+			<dt>Element-wise Operation</dt>
+			<dd>
+				<select bind:value={filter.function}>
+					{#each Object.keys(functions) as fun}
+						<option value={fun}>{fun.charAt(0).toUpperCase()
+  + fun.slice(1)}</option>
+					{/each}
+				</select>
+			</dd>
+		</dl>
+	</fieldset>
+	<h2>Kernel</h2>
+	<Canvas bind:image={filter.mask} brush={brush} />
+</div>
+<div class="image-container">
+		<h2>Filtered Image</h2>
+	<svg style:flx-shrink="0" role="presentation" class="image-array no-cursor" viewBox="0 0 {filteredImage.size.x} {filteredImage.size.y}" width="{filteredImage.size.x}" height="{filteredImage.size.y}" preserveAspectRatio="meet xMidYMid">
+	{#each filteredImage.values as row, y}
+		{#each row as value, x}
+			<rect pointer-events="none" class="intensity" style:--intensity={(value-filteredImageMin)/(filteredImageMax-filteredImageMin)} fill="magenta" image-rendering="crisp-edges" stroke="#abb3" {x} {y} width="1" height=1  vector-effect="non-scaling-stroke" stroke-width="1px"></rect>
+		{/each}
+	{/each}
+	</svg>
+</div>
+	</div>
+</div>
+</div>
---
+++ src/Canvas.svelte
@@ -0,0 +1,252 @@
+<script>
+	export let image = {
+		size: {x:16,y:16},
+		values: Array(16).fill(0).map((v) => Array(16).fill(v))
+	};
+
+	export let range = 0
+	let selectedRange = range
+
+	$: min = range
+	$: max = 1
+
+	$: if(image.values==null||image.values.length != image.size.y || image.values[0].length != image.size.x) {
+		image.values = Array(image.size.y).fill(null).map(() => Array(image.size.x).fill(0))
+	}
+
+
+	$: realMin = min!=null ? min : Math.min.apply(Math, image.values.flat())
+	$: realMax = max!=null ? max :  Math.max.apply(Math, image.values.flat())
+
+	let current = {x:null,y:null}
+	let element;
+	let svgPoint;
+	let active = false
+
+	let widthInput, heightInput;
+
+	export let brush = {
+		value: 1,
+		size: 3,
+		hardness: 0,
+		prev:{x:null,y:null},
+	}
+
+	$: svgPoint = element ? element.createSVGPoint() : null;
+
+	function svgCoords(event) {
+		var ctm = element.getScreenCTM();
+	    // Note: rest of method could work with another element,
+	    // if you don't want to listen to drags on the entire svg.
+	    // But createSVGPoint only exists on <svg> elements.
+		svgPoint.x = event.clientX;
+		svgPoint.y = event.clientY;
+		let {x,y} = svgPoint.matrixTransform(ctm.inverse());
+
+		return {x,y}
+	}
+
+	function clear() {
+		image.size.x = widthInput.valueAsNumber<<0
+		image.size.y = heightInput.valueAsNumber<<0
+		range = selectedRange
+		image.values = Array(image.size.y).fill(null).map(() => Array(image.size.x).fill(0))
+	}
+
+	function startDrawing(evt) {
+		active = true
+		moveDrawing(evt)
+	}
+
+	function stopDrawing() {
+		active = false
+		brush.prev.x = null;
+		brush.prev.y = null;
+	}
+	
+
+	function moveDrawing(evt) {
+		
+		if(!element || !svgPoint) {
+			return
+		}
+
+		let {x,y} = svgCoords(evt);
+		current.x = x
+		current.y = y
+		
+		if(!active) {
+			return
+		}
+		const intX = Math.floor(x);
+		const intY = Math.floor(y);
+		const fracX = x-intX;
+		const fracY = y-intY;
+		if(brush.prev.x==intX && brush.prev.y == intY) {
+			return
+		}
+
+		if(brush.prev.x == null) {
+			placeBrush(intX,intY, fracX, fracY)
+		} else {
+			let signX = 1;
+		  let signY = 1;
+		  const deltaX = Math.abs(intX - brush.prev.x)
+		  const deltaY = Math.abs(intY - brush.prev.y)
+			const m = Math.abs(deltaY/deltaX)
+			if (intX < brush.prev.x) signX = -1;
+			if (intY < brush.prev.y) signY = -1;
+			if (m <= 1) for (let i=0; i <= deltaX; i++) placeBrush( Math.ceil(brush.prev.x + i * signX), Math.ceil(brush.prev.y + i * m * signY), fracX, fracY );
+			else for (let i=0; i<= deltaY; i++) placeBrush( Math.ceil(brush.prev.x + i / m * signX), Math.ceil(brush.prev.y + i * signY), fracX, fracY );
+		}
+		
+		brush.prev.x = intX;
+		brush.prev.y = intY;
+	}
+
+	function placeBrush(x,y, fracX, fracY) {
+		const value = 1*realMin + (realMax - realMin)*brush.value
+		const radius = brush.size/2+0.5;
+		let radius2 = radius*radius;
+		for(let rx=-radius-1;rx<=radius;rx++) {
+			const xx = Math.floor(x+rx);
+			for(let ry=-radius-1;ry<=radius;ry++) {
+				const yy = Math.floor(y+ry);
+				let distance2 = (rx*rx + ry*ry);
+				if(yy < image.size.y && yy >= 0 && xx < image.size.x && xx >= 0 && radius2 > distance2) {
+					const alpha = 1- smoothstep(distance2, (radius2*Math.pow(brush.hardness,16)), radius2)
+					image.values[yy][xx] = image.values[yy][xx] * (1-alpha) + alpha*value
+				}
+			}
+		}
+	}
+
+	function smoothstep (x, edge0 = 0.0, edge1 = 1.0) {
+   // Scale, and clamp x to 0..1 range
+   x = clamp((x - edge0) / (edge1 - edge0));
+
+   return x * x * (3.0 - 2.0 * x);
+}
+
+function clamp(x, lowerlimit = 0.0, upperlimit = 1.0) {
+  if (x < lowerlimit) return lowerlimit;
+  if (x > upperlimit) return upperlimit;
+  return x;
+}
+
+</script>
+
+<style>
+	.image-array {
+		max-width: 18em;
+		max-height: 24em;
+		width: 100%;
+		height: auto;
+		border: 1px solid white;
+	}
+
+	.no-cursor {
+		cursor: none;
+	}
+
+	.intensity {
+		fill: hsl(0deg 0% calc(var(--intensity, 0.5)*100%));
+		accent-color: hsl(0deg 0% calc(var(--intensity, 0.5)*100%));
+	}
+
+	.options {
+		display: grid;
+		grid-template-columns: auto auto;
+		gap: 0.5em 1em;
+	}
+
+   .option-container {
+		display: flex;
+		gap: 1em;
+		border: none;
+		background: #333;
+	}
+
+	.option-container-label {
+		display: inline-block;
+		padding: 2px 0.5em;
+		background: inherit;
+	}
+
+	dd, dt, dl {
+		margin: 0;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		gap: 0.4em;
+		align-items: center;
+		align-content: center;
+		background: #111;
+		color: #fff;
+	}
+
+	dt {
+		align-self: center;
+	}
+
+	dd {
+		align-self: center;
+	}
+
+	input, select, button {
+		margin: 0;
+	}
+
+	ul {
+		display: flex;
+		list-style: none;
+		flex-direction: column;
+		margin: 0;
+		padding: 0;
+		gap: 0.2em;
+	}
+
+	li {
+		margin: 0;
+		padding: 0;
+	}
+</style>
+
+<svelte:document on:mouseup={() => stopDrawing()} on:mousemove={(evt) => moveDrawing(evt)} />
+
+<section class="container">
+
+
+<fieldset class="option-container">
+<legend class="option-container-label">Dimensions</legend>
+<dl class="options">
+<dt>Width</dt>
+<dd><input type="number" size="5" min="3" max="50" step="1" value={image.size.x} bind:this={widthInput}/></dd>
+<dt>Height</dt>
+<dd><input type="number" size="5" min="3" max="50" step="1" value={image.size.y} bind:this={heightInput}/></dd>
+<dt>Value Range</dt>
+<dd>
+	<ul>
+	<li><label><input type="radio" bind:group={selectedRange} value={0} /> Positive (0.0 to 1.0)</label></li>
+	<li><label><input type="radio" bind:group={selectedRange} value={-1} /> Symmetric (-1.0 to 1.0)</label></li>
+</ul>
+</dd>
+<dt></dt>
+<dd><button on:click={clear}>Resize+Clear</button></dd>
+</dl>
+</fieldset>
+
+<svg bind:this={element} class:active={active} role="presentation" on:mousedown={(evt) => startDrawing(evt)} class="image-array no-cursor" viewBox="0 0 {image.size.x} {image.size.y}" width="{image.size.x}" height="{image.size.y}" preserveAspectRatio="meet xMidYMid">
+	{#each image.values as row, y}
+		{#each row as value, x}
+			<rect pointer-events="none" class="intensity" style:--intensity={(value-realMin)/(realMax-realMin)} fill="magenta" image-rendering="crisp-edges" stroke="#abb3" {x} {y} width="1" height=1  vector-effect="non-scaling-stroke" stroke-width="1px"></rect>
+		{/each}
+	{/each}
+	<circle vector-effect="non-scaling-stroke" r={brush.size/2} cx={current.x} cy={current.y} fill="#fffa" stroke="#0005" stroke-width="1px" />
+	<circle vector-effect="non-scaling-stroke" r={brush.size/2*brush.hardness} cx={current.x} cy={current.y} fill="#fffa" />
+</svg>
+
+
+</section>
